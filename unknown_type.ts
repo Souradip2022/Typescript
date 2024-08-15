cb569a35c3518ad1a2b0ba6a3b0a3a70b1b78892
souradipdev <22052939@kiit.ac.in>
2024-08-15 23:39:04 +0530

    Practiced typescript never type

---
+++ unknown_type.ts
@@ -0,0 +1,18 @@
+let val2: unknown = 1; // unknown type will not allow to perform any operation without type checking
+
+if (typeof val2 === "number") {
+  val2++;
+}
+
+if (typeof val2 === "string") {
+  val2.toUpperCase();
+}
+
+if (Array.isArray(val2)) {
+  val2.map(val => val);
+}
+
+if (val2 && typeof val2 === "object" && "foobar" in val2
+  && typeof val2.foobar === "number") {
+  val2.foobar = 2;
+}
